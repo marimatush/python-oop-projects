df574c6683fddb1fc670a1d80358ab0adce7372f
marimatush <marimatush@gmail.com>
2024-06-03 14:22:58 +0200

    Implement geometry classes: Point and Rectangle

---
+++ geometry-game/rectangle.py
@@ -0,0 +1,21 @@
+"""
+Class geometry rectangle.
+"""
+
+
+class Rectangle:
+    """Class geomentry rectangle."""
+
+    def __init__(self, point1, point2):
+        """Initialize with two diagonal points"""
+        self.point1 = point1
+        self.point2 = point2
+
+    def __str__(self):
+        return f"Rectangular defined by \
+                Point1: {self.point1.x}, {self.point1.y} \
+                Point2: {self.point2.x}, {self.point2.y}"
+
+    def area(self):
+        """Calculate area of rectangle."""
+        return (self.point2.x - self.point1.x) * (self.point2.y - self.point1.y)
